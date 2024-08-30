e9d7c926897f30e5d52e53d63b58d13b207c19b0
jiacheng <1363653999@qq.com>
2024-08-30 22:15:02 +0800

    bug被飞神修复，封装了返回给前端的Result类

--- src/main/java/cn/hirrao/javaee/Entity/Result.java
+++ src/main/java/cn/hirrao/javaee/Entity/Result.java
@@ -2,17 +2,19 @@ package cn.hirrao.javaee.Entity;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor  //生成无参构造
 @AllArgsConstructor //生成有参构造
-public class Result {
+@Getter
+public class Result<T> {
     private Integer code;//状态码，0表示成功，-1表示失败
     private String message;//提示信息
-    private Object data;//响应数据，比如token
+    private T data;//响应数据，比如token
 
-    public static Result success(Object data) {
-        return new Result(0,"success",data);
+    public static <E> Result<E> success(E data) {
+        return new Result<>(0,"success",data);
     }
     public static Result success() {
         return new Result(0,"success",null);

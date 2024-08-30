5bf9f835805ea314c46d5a8f90b974f39e6ed015
hirrao <git@hirrao.com>
2024-08-30 12:46:39 +0800

    add token

--- src/main/java/org/health/ErrorPage.java
+++ src/main/java/org/health/ErrorPage.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ErrorPage {
     @GetMapping("/error")
-    public String error(){
+    public String error() {
         return "Error Page";
     }
 }
--- src/main/java/org/health/hirrao/User.java
+++ src/main/java/org/health/hirrao/User.java
@@ -1,26 +1,18 @@
 package org.health.hirrao;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
     private String name;
     private String email;
-    User(String name, String email){
-        this.name=name;
-        this.email=email;
-    }
-    User(){
+    private String id;
+
+    User() {
 
     }
-    public String getName(){
-        return name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-
 }

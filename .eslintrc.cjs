cb418cb14062eea7a87657ea69c9cd03ff93d348
jiacheng <1363653999@qq.com>
2024-08-31 23:08:18 +0800

    欢迎页

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  rules: {
+    'vue/multi-word-component-names': 'off', // 禁用该规则
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',

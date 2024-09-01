a33ef0016feb32bd0b2652dc2228dfb3a169a63e
hirrao <git@hirrao.com>
2024-09-01 18:42:14 +0800

    修改参数

--- src/main/java/cn/hirrao/javaee/controller/BloodPressureController.java
+++ src/main/java/cn/hirrao/javaee/controller/BloodPressureController.java
@@ -3,8 +3,11 @@ package cn.hirrao.javaee.controller;
 import cn.hirrao.javaee.entity.Result;
 import cn.hirrao.javaee.service.BloodPressureService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bp")
@@ -16,8 +19,8 @@ public class BloodPressureController {
     }
 
     @GetMapping("/get/uid")
-    public Result getUid(Long userId) {
-        var result = bloodPressureService.getBloodPressureList(userId);
+    public Result getUid(@RequestBody Map<String, String> map) {
+        var result = bloodPressureService.getBloodPressureList(Long.parseLong(map.get("userId")));
         return Result.success(result);
     }
 }

83ae98f101fa6d2ac4b53adb59fa41d52809f1c0
hirrao <git@hirrao.com>
2024-09-01 15:29:49 +0800

    补全血压实体

--- src/main/java/cn/hirrao/javaee/entity/BloodPressure.java
+++ src/main/java/cn/hirrao/javaee/entity/BloodPressure.java
@@ -1,4 +1,20 @@
 package cn.hirrao.javaee.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class BloodPressure {
+    @TableId(value = "bplld")
+    private Long bplld;
+    private Long uid;
+    private Float SBP;
+    private Float DBP;
+    private String recordTime;
+    private String classification;
+    private String riskLevel;
 }

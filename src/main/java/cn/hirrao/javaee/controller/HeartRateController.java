f65e9cf0e704ab9a1db7111f8b1b51a517659590
hirrao <git@hirrao.com>
2024-09-02 16:59:41 +0800

    修复logger.debug

--- src/main/java/cn/hirrao/javaee/controller/HeartRateController.java
+++ src/main/java/cn/hirrao/javaee/controller/HeartRateController.java
@@ -9,8 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/heartRate")
@@ -19,28 +19,28 @@ public class HeartRateController {
     private final SnowFlake snowFlake = new SnowFlake(1, 3);
     private final HeartRateService heartRateService;
 
-    public HeartRateController(HeartRateService heartRateService){
-        this.heartRateService=heartRateService;
+    public HeartRateController(HeartRateService heartRateService) {
+        this.heartRateService = heartRateService;
     }
 
     @GetMapping("/searchHRById")
-    public Result searchHRById(@RequestBody Map<String, String> map){
-        logger.debug("通过id查找心率信息",map);
-        Long userId=Long.parseLong(map.get("userId"));
+    public Result searchHRById(@RequestBody Map<String, String> map) {
+        logger.debug("通过id查找心率信息{}", map);
+        Long userId = Long.parseLong(map.get("userId"));
         List<HeartRate> result = heartRateService.searchHeartRateById(userId);
         return Result.success(result);
     }
 
     @PostMapping("/insertHR")
-    public Result insertHR(@RequestBody Map<String, String> map){
-        logger.debug("插入心率信息",map);
-        String userId=map.get("userId");
-        String heartRate=map.get("heartRate");
-        String date=map.get("date");
-        if(StringUtil.isEmpty(userId)||StringUtil.isEmpty(heartRate)||StringUtil.isEmpty(date)){
+    public Result insertHR(@RequestBody Map<String, String> map) {
+        logger.debug("插入心率信息{}", map);
+        String userId = map.get("userId");
+        String heartRate = map.get("heartRate");
+        String date = map.get("date");
+        if (StringUtil.isEmpty(userId) || StringUtil.isEmpty(heartRate) || StringUtil.isEmpty(date)) {
             return Result.error(101, "错误输入");
         }
-        heartRateService.insertHeartRate(snowFlake.nextId(),Long.parseLong(userId),Float.parseFloat(heartRate),date);
+        heartRateService.insertHeartRate(snowFlake.nextId(), Long.parseLong(userId), Float.parseFloat(heartRate), date);
         return Result.success();
     }
 }

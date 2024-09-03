8f0ab4f06369237191a89bafa8c322e08494dc7b
hirrao <git@hirrao.com>
2024-09-03 06:19:17 +0000

    格式化所有vue文件 修复build报错

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -14,18 +14,19 @@
                 <p>所在地：北京</p>
                 <p>个性签名：<br> 关注健康，享受生活。</p>
             </div>
-    
+
             <div class="main-content">
                 <div class="stats">
                     <h3>我的日志</h3>
                     <button>增加日志</button>
                 </div>
                 <div class="main">
-                    <div class="editor">      
-                        <quill-editor ref="editorRef" v-model:content="content" :options="options" contentType="html"></quill-editor>
+                    <div class="editor">
+                        <quill-editor ref="editorRef" v-model:content="content" :options="options"
+                            contentType="html"></quill-editor>
                     </div>
                     <div>
-                    <!-- <button @click="print">click me</button> -->
+                        <!-- <button @click="print">click me</button> -->
                     </div>
                 </div>
                 <!-- <div v-html="content" style="width: 100px;"></div> -->
@@ -35,16 +36,16 @@
 </template>
 
 <script setup lang="ts">
-    import router from '../router';
-    import { onMounted, ref,toRaw,nextTick  } from 'vue'
+import router from '../router';
+import { onMounted, ref, toRaw, nextTick } from 'vue'
 //   import filesApi from '@/api/files';
-  import { ElMessage } from 'element-plus'
-  import { Quill, QuillEditor } from '@vueup/vue-quill'
-  import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { ElMessage } from 'element-plus'
+import { Quill, QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
-  const content=ref()
-  const editorRef=ref()
- 
+const content = ref()
+const editorRef = ref()
+
 //   // 处理富文本图片上传
 //   const imageHandler = () => {
 //     // 创建一个文件输入元素
@@ -53,7 +54,7 @@
 //     input.setAttribute('accept', 'image/*');
 //     // 模拟点击，打开文件选择对话框
 //     input.click();
- 
+
 //     // 当用户选择文件后触发的事件
 //     input.onchange = async () => {
 //       // 获取用户选择的文件
@@ -62,11 +63,11 @@
 //         // 创建一个 FormData 对象，用于文件上传
 //         const formData = new FormData();
 //         formData.append('file', file);
- 
+
 //         try {
 //           // 使用 axios 发送 POST 请求，将文件上传到服务器
 //           const response = await axios.post(filesApi.url(), formData, { headers: { 'Content-Type': 'multipart/form-data' } });
- 
+
 //           // 确保获取到 Quill 编辑器实例
 //           const quill = toRaw(editorRef.value).getQuill()
 //           if (quill) {
@@ -81,35 +82,35 @@
 //       }
 //     };
 //   }
- 
-  let options = {
+
+let options = {
     modules: {
-      toolbar: {
-        container: [
-          ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
-          [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
-          [{ align: [] }], // 对齐方式
-          [{ size: ['small', false, 'large', 'huge'] }], // 字体大小
-          [{ font: [] }], // 字体种类
-          [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
-          // [{ direction: 'ltl' }], // 文本方向
-          // [{ direction: 'rtl' }], // 文本方向
-          [{ indent: '-1' }, { indent: '+1' }], // 缩进
-          [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
-          [{ script: 'sub' }, { script: 'super' }], // 上标/下标
-          ['blockquote', 'code-block'], // 引用  代码块
-          ['clean'], // 清除文本格式
-          ['link', 'image', 'video'], // 链接、图片、视频
-        ],
-        handlers: {
-        //   image: imageHandler,
+        toolbar: {
+            container: [
+                ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
+                [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
+                [{ align: [] }], // 对齐方式
+                [{ size: ['small', false, 'large', 'huge'] }], // 字体大小
+                [{ font: [] }], // 字体种类
+                [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
+                // [{ direction: 'ltl' }], // 文本方向
+                // [{ direction: 'rtl' }], // 文本方向
+                [{ indent: '-1' }, { indent: '+1' }], // 缩进
+                [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
+                [{ script: 'sub' }, { script: 'super' }], // 上标/下标
+                ['blockquote', 'code-block'], // 引用  代码块
+                ['clean'], // 清除文本格式
+                ['link', 'image', 'video'], // 链接、图片、视频
+            ],
+            handlers: {
+                //   image: imageHandler,
+            },
         },
-      },
     }
-  }
- 
-  // toolbar标题
-  const titleConfig = [
+}
+
+// toolbar标题
+const titleConfig = [
     { Choice: '.ql-insertMetric', title: '跳转配置' },
     { Choice: '.ql-bold', title: '加粗' },
     { Choice: '.ql-italic', title: '斜体' },
@@ -154,148 +155,166 @@
     { Choice: '.ql-align .ql-picker-item[data-value="center"]', title: '居中对齐' },
     { Choice: '.ql-align .ql-picker-item[data-value="right"]', title: '居右对齐' },
     { Choice: '.ql-align .ql-picker-item[data-value="justify"]', title: '两端对齐' }
-  ]
- 
-  // 给富文本框工具栏加上鼠标悬浮中文提示
-  const initTitle = () => {
+]
+
+// 给富文本框工具栏加上鼠标悬浮中文提示
+const initTitle = () => {
     for (let item of titleConfig) {
-      // .editor 是富文本编辑器的类名
-      let tip = document.querySelector('.editor ' + item.Choice);
-      if (tip) {
-        tip.setAttribute('title', item.title);
-      }
-    } 
-  }
- 
-  onMounted(async()=>{
+        // .editor 是富文本编辑器的类名
+        let tip = document.querySelector('.editor ' + item.Choice);
+        if (tip) {
+            tip.setAttribute('title', item.title);
+        }
+    }
+}
+
+onMounted(async () => {
     // 确保 Quill 编辑器已完全加载
     await nextTick();
     // 初始化工具栏的中文提示
     initTitle();
-  })
+})
 
 
 </script>
 
 <style>
-    .main-profile {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #1e1e1e;
-        color: #ffffff;
-    }
-    .editor {
-        background-color: white;
-        overflow: auto;
-        resize:both;
-        max-width: 99%;
-        min-height: 600px;
-        color: #000;
-    }
-    
-    .profile {
-        display: flex;
-        margin: 20px;
-        align-items: stretch;
-    }
-    .profile .sidebar {
-        width: 25%;
-        background-color: #2b2b2b;
-        padding: 20px;
-        border-radius: 10px;
-    }
-    .intro {
-        width: 25%;
-        margin: 20px;
-        display: flex;
-    }
-    .intro .detail h2 {
-        margin: 5px;
-    }
-    .intro .detail p {
-        margin: 5px;
-    }
-    .intro img {
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-    }
-    .profile .sidebar h2 {
-        font-size: 20px;
-        margin: 10px 0;
-    }
-    .profile .sidebar p {
-        margin: 5px 0;
-    }
-    .profile .sidebar a {
-        font-size: large;
-        color: #000;
-        margin-bottom: 10px;
-    }
-    .profile .sidebar button {
-        background-color: #9b9b9b;
-        color: white;
-        border: none;
-        padding: 10px 15px;
-        border-radius: 5px;
-        cursor: pointer;
-        width: 100%;
-        margin-top: 10px;
-    }
-    .profile .main-content {
-        width: 70%;
-        margin-left: 5%;
-        min-height: 500px;
-    }
-    .stats {
-        background-color: #2b2b2b;
-        padding: 20px;
-        border-radius: 10px;
-        margin-bottom: 20px;
-        display: flex;
-        justify-content: space-between;
-    }
-    .stats h3 {
-        margin: 0 0 10px 0;
-        font-size: 18px;
-    }
-    .stats button {
-        background-color: #d6e105;
-        color: rgb(7, 1, 1);
-        font-weight: bolder;
-        border: none;
-        padding: 10px 15px;
-        border-radius: 5px;
-        cursor: pointer;
-        width: 20%;
-        margin-top: 10px;
-    }
+.main-profile {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #1e1e1e;
+    color: #ffffff;
+}
 
-    .footer {
-        text-align: center;
-        padding: 10px;
-        background-color: #2c2c2c;
-        margin-top: 20px;
-    }
-    .fav {
-        margin: 10px;
-        display: flex;
-        align-items: center;
-        padding: 20px, 20px;
-        justify-content: center; 
-    }
-    .fav .u{
-        text-decoration: none;
-    }
-    .fav .sep {
-        color: #9b9b9b;
-        font-size: 40px;
-    }
-    #container {
-        width: 90%;
-        height: 500px;
-        margin: 10px;
-        padding: 10px;
-    }
+.editor {
+    background-color: white;
+    overflow: auto;
+    resize: both;
+    max-width: 99%;
+    min-height: 600px;
+    color: #000;
+}
+
+.profile {
+    display: flex;
+    margin: 20px;
+    align-items: stretch;
+}
+
+.profile .sidebar {
+    width: 25%;
+    background-color: #2b2b2b;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+.intro {
+    width: 25%;
+    margin: 20px;
+    display: flex;
+}
+
+.intro .detail h2 {
+    margin: 5px;
+}
+
+.intro .detail p {
+    margin: 5px;
+}
+
+.intro img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+}
+
+.profile .sidebar h2 {
+    font-size: 20px;
+    margin: 10px 0;
+}
+
+.profile .sidebar p {
+    margin: 5px 0;
+}
+
+.profile .sidebar a {
+    font-size: large;
+    color: #000;
+    margin-bottom: 10px;
+}
+
+.profile .sidebar button {
+    background-color: #9b9b9b;
+    color: white;
+    border: none;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 100%;
+    margin-top: 10px;
+}
+
+.profile .main-content {
+    width: 70%;
+    margin-left: 5%;
+    min-height: 500px;
+}
+
+.stats {
+    background-color: #2b2b2b;
+    padding: 20px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.stats h3 {
+    margin: 0 0 10px 0;
+    font-size: 18px;
+}
+
+.stats button {
+    background-color: #d6e105;
+    color: rgb(7, 1, 1);
+    font-weight: bolder;
+    border: none;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 20%;
+    margin-top: 10px;
+}
+
+.footer {
+    text-align: center;
+    padding: 10px;
+    background-color: #2c2c2c;
+    margin-top: 20px;
+}
+
+.fav {
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    padding: 20px, 20px;
+    justify-content: center;
+}
+
+.fav .u {
+    text-decoration: none;
+}
+
+.fav .sep {
+    color: #9b9b9b;
+    font-size: 40px;
+}
+
+#container {
+    width: 90%;
+    height: 500px;
+    margin: 10px;
+    padding: 10px;
+}
 </style>

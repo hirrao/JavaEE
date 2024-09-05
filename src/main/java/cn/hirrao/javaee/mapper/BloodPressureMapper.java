b699b33672afcc1b1f5d9bed2ed196111c214518
hirrao <git@hirrao.com>
2024-09-05 15:34:20 +0800

    update

--- src/main/java/cn/hirrao/javaee/mapper/BloodPressureMapper.java
+++ src/main/java/cn/hirrao/javaee/mapper/BloodPressureMapper.java
@@ -13,6 +13,6 @@ public interface BloodPressureMapper extends BaseMapper<BloodPressure> {
     @Select("SELECT DATE(recordTime) as recordDate, AVG(SBP) as SBP , AVG(DBP) as DBP FROM bloodPressureLog WHERE uid= #{uid} AND DATE(recordTime) BETWEEN DATE_SUB(#{date}, INTERVAL 6 DAY)  AND #{date} GROUP BY DATE(recordTime)")
     List<BloodPressure> getBloodPressureList(Long uid, String date);
 
-    @Insert("INSERT INTO bloodPressureLog(bplId, uid, SBP, DBP, recordTime, classification, riskLevel) VALUES(#{bplId},#{uid}, #{SBP}, #{DBP}, #{recordTime}, #{classification}, #{riskLevel})")
+    @Insert("INSERT INTO bloodPressureLog(bplId, uid, SBP, DBP, recordTime, classification, riskLevel) VALUES(#{bplId},#{uid}, #{sbp}, #{dbp}, #{recordTime}, #{classification}, #{riskLevel})")
     void insertBloodPressure(long bplId, long uid, String date, float sbp, float dbp, String classification, String riskLevel);
 }

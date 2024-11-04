111505467f0f53d08060648448d6accb84fc6cfe
hirrao <git@hirrao.com>
2024-11-05 01:58:12 +0800

    feat(web): 引入Eslint(并未完全配置结束)

---
+++ web/eslint.config.mjs
@@ -0,0 +1,24 @@
+import pluginVue from 'eslint-plugin-vue'
+import prettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+import vueEslintParser from 'vue-eslint-parser'
+import { parser } from 'typescript-eslint'
+
+export default [
+ ...pluginVue.configs['flat/recommended'],
+ prettier,
+ ...tseslint.configs['recommended'],
+  {
+    languageOptions: {
+      parser: vueEslintParser, 
+      parserOptions: {
+        parser: parser,
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+    rules:{
+
+    }
+  }
+]
